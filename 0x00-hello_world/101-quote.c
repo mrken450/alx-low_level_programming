94c972c729dc27a32329cc4536e2469eec6de0b2
mrken450 <knndumale@gmail.com>
2022-12-09 17:47:02 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print exactly and that place of art is useful
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	write(2, "and that place of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	return (1);
+}
